0cef8700480995243c95bee835e06507d7f91a9f
chenlingjie <chenlingjie@cd121.com>
2017-07-17 10:52:15 +0800

    添加富文本编辑器simditor

---
+++ editor/index.js
@@ -0,0 +1,114 @@
+/**
+ * Created by xiaobxia on 2017/7/17.
+ */
+var editor = new Simditor({
+    //挂载的元素
+    textarea: $('#editor'),
+    placeholder: '',
+    defaultImage: 'images/image.png',
+    params: {},
+    upload: {
+        url: '',
+        params: null,
+        fileKey: 'upload_file',
+        connectionCount: 3,
+        leaveConfirm: 'Uploading is in progress, are you sure to leave this page?'
+    },
+    tabIndent: true,
+    //工具条
+    toolbar: [
+        'title',
+        'bold',
+        'italic',
+        'underline',
+        'strikethrough',
+        'fontScale',
+        'color',
+        'ol',
+        'ul',
+        'blockquote',
+        'code',
+        'table',
+        'link',
+        'image',
+        'hr',
+        'indent',
+        'outdent',
+        'alignment'
+    ],
+    toolbarFloat: true,
+    toolbarFloatOffset: 0,
+    toolbarHidden: false,
+    pasteImage: false,
+    cleanPaste: false,
+    //null
+    allowedTags: ['br', 'span', 'a', 'img', 'b', 'strong', 'i', 'strike', 'u', 'font', 'p', 'ul', 'ol', 'li', 'blockquote', 'pre', 'code', 'h1', 'h2', 'h3', 'h4', 'hr'],
+    //null
+    allowedAttributes: {
+        img: ['src', 'alt', 'width', 'height', 'data-non-image'],
+        a: ['href', 'target'],
+        font: ['color'],
+        code: ['class']
+    },
+    //null
+    allowedStyles: {
+        span: ['color', 'font-size'],
+        b: ['color'],
+        i: ['color'],
+        strong: ['color'],
+        strike: ['color'],
+        u: ['color'],
+        p: ['margin-left', 'text-align'],
+        h1: ['margin-left', 'text-align'],
+        h2: ['margin-left', 'text-align'],
+        h3: ['margin-left', 'text-align'],
+        h4: ['margin-left', 'text-align']
+    },
+    //null
+    codeLanguages: [
+        {name: 'Bash', value: 'bash'},
+        {name: 'C++', value: 'c++'},
+        {name: 'C#', value: 'cs'},
+        {name: 'CSS', value: 'css'},
+        {name: 'Erlang', value: 'erlang'},
+        {name: 'Less', value: 'less'},
+        {name: 'Sass', value: 'sass'},
+        {name: 'Diff', value: 'diff'},
+        {name: 'CoffeeScript', value: 'coffeescript'},
+        {name: 'HTML,XML', value: 'html'},
+        {name: 'JSON', value: 'json'},
+        {name: 'Java', value: 'java'},
+        {name: 'JavaScript', value: 'js'},
+        {name: 'Markdown', value: 'markdown'},
+        {name: 'Objective C', value: 'oc'},
+        {name: 'PHP', value: 'php'},
+        {name: 'Perl', value: 'parl'},
+        {name: 'Python', value: 'python'},
+        {name: 'Ruby', value: 'ruby'},
+        {name: 'SQL', value: 'sql'}
+    ]
+});
+
+//方法
+// editor.setValue();
+// editor.getValue();
+// editor.sync();
+// editor.focus();
+// editor.blur();
+// editor.hidePopover();
+// editor.destroy();
+
+//事件
+editor.on('valuechanged',function (e,src) {
+});
+editor.on('selectionchanged',function () {
+});
+editor.on('decorate',function (e,$el) {
+});
+editor.on('undecrate',function () {
+});
+editor.on('pasting',function () {
+});
+//focus,blur,destroy
+
+
---
+++ editor/scripts/readme.md
@@ -0,0 +1,13 @@
+### 文件引入
+1.使用非合并文件(需要按顺序)
+``` html
+<script src="scripts/module.js"></script>
+<script src="scripts/hotkeys.js"></script>
+<script src="scripts/uploader.js"></script>
+<script src="scripts/=simditor.js"></script>
+```
+
+2.使用合并文件
+``` html
+<script src="scripts/all/simditor.js"></script>
+```
